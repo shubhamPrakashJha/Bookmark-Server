c3f77278884a008f5052ed027680fb37a2cef880
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2017-10-16 15:52:53 +0530

    store html form in  variable "form"

--- bookmarkserver.py
+++ bookmarkserver.py
@@ -3,6 +3,25 @@ import http.server
 import requests
 from urllib.parse import unquote, parse_qs
 
+form = '''<!DOCTYPE html>
+<title>Bookmark Server</title>
+<form method="POST">
+    <label>Long URI:
+        <input name="longuri">
+    </label>
+    <br>
+    <label>Short name:
+        <input name="shortname">
+    </label>
+    <br>
+    <button type="submit">Save it!</button>
+</form>
+<p>URIs I know about:
+<pre>
+{}
+</pre>
+'''
+
 if __name__ == '__main__':
     server_address = ('', 8000)
     httpd = http.server.HTTPServer(server_address, Shortener)
