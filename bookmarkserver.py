aa0d1df148541dcfa739cb0261c8a166326ac078
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2017-10-16 15:57:41 +0530

    initialize do_GET method

--- bookmarkserver.py
+++ bookmarkserver.py
@@ -25,6 +25,25 @@ form = '''<!DOCTYPE html>
 '''
 class Shortener(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        name = unquote(self.path[1:])
+
+        if name:
+            if name in memory:
+                self.send_response(303)
+                self.send_header('Location', memory[name])
+                self.end_headers()
+            else:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain; charset=utf-8')
+                self.end_headers()
+                self.wfile.write("I don't know '{}'.".format(name).encode())
+        else:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            known = "\n".join("{} : {}".format(key, memory[key])
+                              for key in sorted(memory.keys()))
+            self.wfile.write(form.format(known).encode())
 
     def do_POST(self):
 
