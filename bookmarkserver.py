f785e690e1b312356c70701aee3c132d369973b7
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2017-10-16 15:50:00 +0530

    create main function

--- bookmarkserver.py
+++ bookmarkserver.py
@@ -2,3 +2,8 @@
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
+
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd.serve_forever()
